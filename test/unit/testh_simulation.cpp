658428ead6571e070bb4b47cd9ff600e3b2e64cb
LiamBlake <liamlemblake@gmail.com>
2021-03-21 11:06:11 +1030

    forgot namespace

--- test/unit/testh_simulation.cpp
+++ test/unit/testh_simulation.cpp
@@ -111,10 +111,10 @@ BOOST_AUTO_TEST_CASE(testfeature_followon) {
 
   // Ensure fit matches that expected by R
   double eps = 0.0001;
-  BOOST_TEST(abs(MODEL_FOLLOW_ON(200) - 0.1386838) < eps);
-  BOOST_TEST(abs(MODEL_FOLLOW_ON(250) - 0.3812311) < eps);
-  BOOST_TEST(abs(MODEL_FOLLOW_ON(350) - 0.7442012) < eps);
-  BOOST_TEST(abs(MODEL_FOLLOW_ON(500) - 0.9046413) < eps)
+  BOOST_TEST(abs(Match::MODEL_FOLLOW_ON(200) - 0.1386838) < eps);
+  BOOST_TEST(abs(Match::MODEL_FOLLOW_ON(250) - 0.3812311) < eps);
+  BOOST_TEST(abs(Match::MODEL_FOLLOW_ON(350) - 0.7442012) < eps);
+  BOOST_TEST(abs(Match::MODEL_FOLLOW_ON(500) - 0.9046413) < eps)
 }
 
 BOOST_AUTO_TEST_SUITE_END()

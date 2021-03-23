b40cfcdf5ed719f05ecafd1664a8bebf7c60845d
Liam Blake <37836658+LiamBlake@users.noreply.github.com>
2021-03-23 21:13:56 +1030

    Added new files to cmake

--- test/unit/testh_simulation.cpp
+++ test/unit/testh_simulation.cpp
@@ -13,6 +13,7 @@
  public // This is a hack and immensely evil, but lets me test private methods
         // easily
 #include "Cards.h"
+#include "Model.h"
 #include "Player.h"
 #include "Simulation.h"
 
@@ -110,10 +111,10 @@ BOOST_AUTO_TEST_CASE(testfeature_followon) {
 
   // Ensure fit matches that expected by R
   double eps = 0.0001;
-  BOOST_TEST(abs(Match::MODEL_FOLLOW_ON(200) - 0.1386838) < eps);
-  BOOST_TEST(abs(Match::MODEL_FOLLOW_ON(250) - 0.3812311) < eps);
-  BOOST_TEST(abs(Match::MODEL_FOLLOW_ON(350) - 0.7442012) < eps);
-  BOOST_TEST(abs(Match::MODEL_FOLLOW_ON(500) - 0.9046413) < eps);
+  BOOST_TEST(abs(Model::MODEL_FOLLOW_ON(200) - 0.1386838) < eps);
+  BOOST_TEST(abs(Model::MODEL_FOLLOW_ON(250) - 0.3812311) < eps);
+  BOOST_TEST(abs(Model::MODEL_FOLLOW_ON(350) - 0.7442012) < eps);
+  BOOST_TEST(abs(Model::MODEL_FOLLOW_ON(500) - 0.9046413) < eps);
 }
 
 BOOST_AUTO_TEST_SUITE_END()

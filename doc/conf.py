fb3e2ec64c05127a8ad287196263302a0bf38566
LiamBlake <liamlemblake@gmail.com>
2021-04-27 20:26:06 +0930

    Test index

--- doc/conf.py
+++ doc/conf.py
@@ -1,12 +1,12 @@
 import subprocess, os
 
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+read_the_docs_build = os.environ.get("READTHEDOCS", None) == "True"
 
 if read_the_docs_build:
-    subprocess.call('cd ../doxygen; doxygen', shell=True)
+    subprocess.call("cd ../doxygen; doxygen", shell=True)
 
 breathe_projects = {
-    "testmatch":"xml",
-    }
-    
-extension = ["breathe"]
+    "testmatch": "xml",
+}
+
+extensions = ["breathe"]

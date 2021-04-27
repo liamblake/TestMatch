5faa8ee2b3beebc643d970612e1dd9529fdfe0ca
LiamBlake <liamlemblake@gmail.com>
2021-04-27 20:30:48 +0930

    Add doc requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ import subprocess, os
 read_the_docs_build = os.environ.get("READTHEDOCS", None) == "True"
 
 if read_the_docs_build:
-    subprocess.call("cd ../doxygen; doxygen", shell=True)
+    subprocess.call("doxygen", shell=True)
 
 breathe_projects = {
     "testmatch": "xml",

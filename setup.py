d5b95c811e696e6941d8facae5b398dd5f9c59fb
LiamBlake <liamlemblake@gmail.com>
2021-06-03 15:33:35 +0930

    Specify compiled library as package_data

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ setup(
     author_email="",
     packages=find_packages("src"),
     package_dir={"": "src"},
+    package_data={
+        "": "build/src/cpp/bindings/_testmatch.so"
+    },  # TODO: Remove this hard-coding, instead compile library on install.
+    # See https://docs.python.org/2/distutils/setupscript.html#describing-extension-modules
     install_requires=["rich"],
     extras_require={"dev": ["black", "flake8", "pytest", "pytest-cov"]},
     version="0.1",

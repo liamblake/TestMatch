040e8391b168436c347516e7fa9a0f154420ce4c
LiamBlake <liamlemblake@gmail.com>
2021-06-03 15:36:13 +0930

    Fix specification

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     package_data={
-        "": "build/src/cpp/bindings/_testmatch.so"
+        "": ["build/src/cpp/bindings/_testmatch.so"]
     },  # TODO: Remove this hard-coding, instead compile library on install.
     # See https://docs.python.org/2/distutils/setupscript.html#describing-extension-modules
     install_requires=["rich"],

049af0f82273e8b4a9af188dfc2d9861b829f210
LiamBlake <liamlemblake@gmail.com>
2021-06-10 10:18:17 +0930

    Add versions to dev dependencies

--- setup.py
+++ setup.py
@@ -9,8 +9,17 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=["rich"],
-    extras_require={"dev": ["black", "flake8", "pytest", "pytest-cov", "isort"]},
-    cmake_args=["-DBUILD_PYTHON=ON", "-GUnix Makefiles"],
+    extras_require={
+        "dev": [
+            "black==21.5b2",
+            "flake8==3.9.2",
+            "pytest==6.2.4",
+            "pytest-cov==2.12.1",
+            "isort==5.8.0",
+            "mypy==0.901",
+        ]
+    },
+    cmake_args=["-DBUILD_PYTHON=ON"],
     cmake_install_dir="src/testmatch",
     include_package_data=True,
     version="0.1",
